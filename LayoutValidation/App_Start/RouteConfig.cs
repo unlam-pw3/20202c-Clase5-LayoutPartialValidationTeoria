d889b1dac36ba7e31799e5a09cdca386597a25ee
juizmariano <juizmariano@gmail.com>
2020-09-29 23:23:01 -0300

    Cambio 1

--- LayoutValidation/App_Start/RouteConfig.cs
+++ LayoutValidation/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace LayoutValidation
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Empleado", action = "Index", id = UrlParameter.Optional }
             );
 
 
